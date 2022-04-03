9d8d95cf4d662a503cf0a866e41b8109aa4f8303
ifty03 <ashikulislamb84@gmail.com>
2022-04-04 02:37:12 +0600

    review added

--- src/Components/DefaultReviews/DefaultReviews.js
+++ src/Components/DefaultReviews/DefaultReviews.js
@@ -1,12 +1,16 @@
 import React from "react";
-import useReview from "../hooks/hooks";
 
-const DefaultReviews = () => {
-  const [reviews, setReviews] = useReview();
-  console.log(reviews);
+const DefaultReviews = ({ comment }) => {
+  const { address, ratting, email, name, phone, picture, review } = comment;
   return (
-    <div>
-      <h1>this is default reviews</h1>
+    <div className="w-96 bg-purple-200 mt-4 p-7 rounded-lg">
+      <img className="mx-auto rounded-full" src={picture} alt="" />
+      <h1 className="text-xl font-bold font-mono mt-3">{name}</h1>
+      <h1 className="text-xl font-sans">{email}</h1>
+      <h2 title={review} className="font-semibold mt-3">
+        {review.length > 200 ? review.slice(0, 200) + "..." : review}
+      </h2>
+      <h3>Ratting: {ratting} star</h3>
     </div>
   );
 };
